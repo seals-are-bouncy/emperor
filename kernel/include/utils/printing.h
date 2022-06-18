c5e5ff88af2d2a5d138d02c0e40edaff33c1ddd2
SirObby <70753342+SirObby@users.noreply.github.com>
2022-06-18 21:25:05 +0800

    oop

---
+++ kernel/include/utils/printing.h
@@ -0,0 +1,10 @@
+#if !defined (PRINTING_H)
+#define PRINTING_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+void update_cursor(int x, int y);
+
+#endif
---
+++ kernel/src/utils/printing.c
@@ -0,0 +1,20 @@
+#include "utils/printing.h"
+
+static inline void outb(uint16_t port, uint8_t val)
+{
+    asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
+    /* There's an outb %al, $imm8  encoding, for compile-time constant port numbers that fit in 8b.  (N constraint).
+     * Wider immediate constants would be truncated at assemble-time (e.g. "i" constraint).
+     * The  outb  %al, %dx  encoding is the only option for all other cases.
+     * %1 expands to %dx because  port  is a uint16_t.  %w1 could be used if we had the port number a wider C type */
+}
+
+void update_cursor(int x, int y)
+{
+	uint16_t pos = y * 80 + x;
+ 
+	outb(0x3D4, 0x0F);
+	outb(0x3D5, (uint8_t) (pos & 0xFF));
+	outb(0x3D4, 0x0E);
+	outb(0x3D5, (uint8_t) ((pos >> 8) & 0xFF));
+}
